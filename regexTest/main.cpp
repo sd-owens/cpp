fb0c97d88357e8a6f608531b963c5b015178de06
Steve Owens <sd.owens@outlook.com>
2019-09-28 20:32:28 -0700

    finalized regex tests

--- regexTest/main.cpp
+++ regexTest/main.cpp
@@ -2,14 +2,38 @@
 #include <string>
 #include <regex>
 
-int main()
-{
-    std::string str("1231");
-    std::regex r("^(\\d)\\d"); // entire match will be 2 numbers
+int main() {
+
+    std::string str("1");
+
+    std::regex r("^\\d+$"); 
 
     std::smatch m;
     std::regex_search(str, m, r);
 
-    for(auto v: m)
-        std::cout << v << std::endl;
+    std::cout << "after search, match was " 
+              << (!m.empty() ? " true\n" : " not false\n");
+
+    // std::regex r("^\\d+$"); 
+
+    // std::smatch m;
+    // std::regex_search(str, m, r);
+
+    // for(auto v: m) {
+    //     std::cout << v << std::endl;
+    // }
+    // std::cout << "after search, match was " 
+    //           << (!m.empty() ? " found\n" : " not found\n");
+
+    // std::string str("4");
+    // std::regex r("^[2-3]$"); 
+
+    // std::smatch m;
+    // std::regex_search(str, m, r);
+
+    // for(auto v: m) {
+    //     std::cout << v << std::endl;
+    // }
+    // std::cout << "after search, match was " 
+    //           << (!m.empty() ? " found\n" : " not found\n");
 }
